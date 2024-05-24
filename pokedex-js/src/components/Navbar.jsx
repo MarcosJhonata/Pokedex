c4be7a01d2a45784167c94955829e2b636afa7d5
MarcosJhonata <marcosjhonata23@gmail.com>
2024-05-24 16:30:16 -0300

    alteração na navbar para buscar e implementação inicial do search

--- pokedex-js/src/components/Navbar.jsx
+++ pokedex-js/src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import style from "./Navbar.module.css"
 import {BiSearchAlt2} from 'react-icons/bi'
+import { useState } from "react";
 
 export function Navbar () {
+
+    const [search , setSearch] = useState(" ");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!search) return;
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    }
     return (
         <nav className={style.navigation}>
             <h2>
              <Link to="/">Pokedex</Link>
             </h2>
-            <form className={style.search}>
-                <input type="text" placeholder="Procure um Pokemon" />
+            <form onSubmit={handleSubmit} className={style.search}>
+                <input 
+                type="text" 
+                placeholder="Procure um Pokemon"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search} />
                 <button type="submit">
                 <BiSearchAlt2/>
                 </button>
--- pokedex-js/src/pages/Search.jsx
+++ pokedex-js/src/pages/Search.jsx
@@ -1,6 +1,26 @@
+import { useEffect , useState } from "react";
+import {useSearchParams} from "react-router-dom";
+import Pokemon from "../components/Pokemon";
+import style from "./Home.module.css"
+import axios from "axios";
+
 export function Search () {
-    return(
-        <h2>oi</h2>
-    )
+  const [searchParams] = useSearchParams();
+  const [pokemon,setPokemon] = useState([]);
+  const query = searchParams.get("q");
+
+  useEffect(() => {
+      axios.get('https://pokeapi.co/api/v2/pokemon/name')
+      .then((response) => setPokemon (response.name.results))
+  },[]);   
+
+    return (
+      <div className={style.container}>
+        <h2>Resultados para: <span>{query}</span></h2>
+             {pokemon.map((item) => (
+            <Pokemon key={item.data} data={item}/>
+        ))}      
+        </div> 
+     ) ;
 }
 export default Search
